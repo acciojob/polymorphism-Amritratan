f5f24a68f5308b0ecb8fe1d0c810f736382d21dc
SnakeGame <amritratan123@gmail.com>
2023-05-14 14:08:27 +0530

    Finalized

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,23 +6,23 @@ public class Main {
         //Task 3
         public  int product(int x, int y) { //call to main function object p and return a value
 
-            return x;
+            return x*y;
         }
         //Task 4
         public int product(int x, int y, int z) {  //call to main function object p and return a value
-            return x;
+            return x*y*z;
         }
         //Task 5
         public double product(double x, double y) {   //call to main function object p and return the return a value
-            return x;
+            return x*y;
         }
     }
     public static void main(String[] Args) {
         //Task 2
         Product p=new Product();
-        p.product(1,2); //Task 3, called value by product method
-        p.product(1,2,3); //Task 4, called value by product method
-        p.product(0.2,0.8);  //Task 5, called value by product method
+        System.out.println(p.product(1,2)); //Task 3, called value by product method
+        System.out.println(p.product(1,2,3));; //Task 4, called value by product method
+        System.out.println(p.product(0.2,0.5));  //Task 5, called value by product method
     }
 
 }
